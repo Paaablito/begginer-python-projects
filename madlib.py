75b674e97b021bb0c933b91b83800565d6b9c445
Paaablito <108811993+Paaablito@users.noreply.github.com>
2022-07-12 20:20:19 +0200

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+
+sentence = f"His palms are {adj1}, knees weak, arms are heavy There's vomit on his sweater already, mom's spaghetti \
+He's nervous, but on the surface he looks {adj2} and ready \
+To drop bombs, but he keeps on forgettin' \
+What he wrote down, the whole crowd goes so loud"
+
+print(sentence)
